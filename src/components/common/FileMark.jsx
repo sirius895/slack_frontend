aad4fef7351f5caea0061a30cc0eb9ec08fcedd6
siriusblack <siriusblack@gmail.com>
2025-09-17 09:02:28 -0700

    fix:FileMark

--- src/components/common/FileMark.jsx
+++ src/components/common/FileMark.jsx
@@ -1,15 +1,20 @@
-import { HStack, Image, VStack } from "@chakra-ui/react";
+import { HStack, Image, Link, VStack } from "@chakra-ui/react";
 import { FaDownload } from "react-icons/fa";
 
 const FileMark = (props) => {
   const { originalname, filename, w = "56px", h = "56px", ...etcProps } = props;
   let url = `${process.env.REACT_APP_BASE_URL}/files/${filename ? filename : "default.gif"}`;
+  const downloadURL = `${process.env.REACT_APP_BASE_URL}/file/download/${filename}/${originalname}`;
 
   return (
     <VStack gap={2}>
-      <Image src={url} w={w} h={h} rounded={"8px"} {...etcProps} />
+      <Link href={url} target="_blank">
+        <Image src={url} w={w} h={h} rounded={"8px"} {...etcProps} />
+      </Link>
       <HStack w={"full"} justify={"center"} align={"center"}>
-        <FaDownload />
+        <Link href={downloadURL}>
+          <FaDownload />
+        </Link>
       </HStack>
     </VStack>
   );
